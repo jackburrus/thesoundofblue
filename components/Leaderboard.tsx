04efd089a55680071350c20dd4a5959ed7c12e0d
jack <jhburrus3@gmail.com>
2023-06-27 12:31:23 -0600

    table styling coming along

--- components/Leaderboard.tsx
+++ components/Leaderboard.tsx
@@ -11,6 +11,7 @@ const columnHelper = createColumnHelper<PhrasePairRow>();
 export const columns = [
 	columnHelper.display({
 		id: 'voteButtons',
+		header: 'Vote',
 		cell: (props) => <VoteButtons row={props.row} />,
 	}),
 	columnHelper.accessor('phraseOne', {
@@ -24,8 +25,8 @@ export const columns = [
 export default function LeaderBoardTable({ data }: { data: PhrasePairRow[] }) {
 	const tableInstance = useReactTable({ columns, data, getCoreRowModel: getCoreRowModel() });
 	return (
-		<div className="w-full">
-			<table>
+		<div className="border border-green-500 items-center justify-center flex h-1/2 w-3/4 ">
+			<table className="border border-red-500 flex flex-col  ">
 				<thead>
 					{tableInstance.getHeaderGroups().map((headerGroup) => (
 						<tr key={headerGroup.id}>
@@ -37,7 +38,7 @@ export default function LeaderBoardTable({ data }: { data: PhrasePairRow[] }) {
 						</tr>
 					))}
 				</thead>
-				<tbody>
+				<tbody className="">
 					{tableInstance.getRowModel().rows.map((row) => (
 						<tr key={row.id}>
 							{row.getVisibleCells().map((cell) => (
